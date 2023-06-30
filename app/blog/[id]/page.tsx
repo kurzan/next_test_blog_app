574a9c31369c749306d832880caded6a914385af
Kurzov <kurzanbl4@gmail.com>
2023-06-30 13:10:20 +0300

    getData

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,16 @@
 import { Metadata } from "next";
 
+async function getData(id: string) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: {
+      revalidate: 60
+    }
+  });
+
+  return response.json();
+}
+
+
 type Props = {
   params: {
     id: string;
@@ -7,13 +18,21 @@ type Props = {
 };
 
 export async function generateMetadata({params: {id}}: Props): Promise<Metadata> {
+  const post = await getData(id);
+
   return {
-    title: id
+    title: post.title
   }
 };
 
-export default function Post({params: {id}}: Props) {
+
+export default async function Post({params: {id}}: Props) {
+  const post = await getData(id);
+
   return(
-    <h1>Post page {id}</h1>
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
   )
 };
