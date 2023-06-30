603b46e7d88b53ccd1ccd8c785fcc427852d0da3
Kurzov <kurzanbl4@gmail.com>
2023-06-30 12:50:33 +0300

    metadata title and generate title

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from "next";
+
 type Props = {
   params: {
     id: string;
   }
-}
+};
+
+export async function generateMetadata({params: {id}}: Props): Promise<Metadata> {
+  return {
+    title: id
+  }
+};
 
 export default function Post({params: {id}}: Props) {
   return(
